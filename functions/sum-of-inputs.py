e85bb4d6f3b4921869b787697d9f9543364810a4
Kandasamy <selvaraj.kandasamy.ext@nokia.com>
2018-11-28 14:35:16 +0530

    removed expre c=x+y in function to find the types of arg and sum

--- functions/sum-of-inputs.py
+++ functions/sum-of-inputs.py
@@ -2,8 +2,7 @@ def sum_of_inputs(x,y):                 # To calculate the sume of given value.
 	if (type(x) != int or type(y) !=int) and (type(x) != str or type(y) != str):
 		return -1
 	else:
-		c=x+y
-		return c		
+		return x+y		
 
 if __name__ == '__main__':
 	a,b=input("enter a two values to be sum :")		
