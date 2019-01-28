322cccfa07d621cb4961c23220e8b4eace31c878
Kandasamy <selvaraj.kandasamy.ext@nokia.com>
2019-01-28 21:46:17 +0530

    Changed as per comments, removed ordered dict and zip function implemented

--- functions/frequency_of_characters.py
+++ functions/frequency_of_characters.py
@@ -1,30 +1,31 @@
-from collections import OrderedDict
 def frequency_of_characters(string):
-	"""function to count the number of times of each character and return as dictionary.
+  """function to count the number of times of each character and prints the output.
     Args:
        string(str): The first parameter.
 
-    Returns:
-        Dict: The return value, ordered dictionary with key - character and value - count of character.
-
     Example:
-    	string='Example' |  [('E', 1), ('x', 1), ('a', 1), ('m', 1), ('p', 1), ('l', 1), ('e', 1)]
-    """	
-	count=0
-	Dict=OrderedDict()
-	string=string.replace(' ','')
-	print string
-   	for char in string:
-   		count=string.count(char)
-   		Dict[char]=count
-   	#ordict=collections.OrderedDict(Dict)
- 	return Dict
+      string='Example' |  [('E', 1), ('x', 1), ('a', 1), ('m', 1), ('p', 1), ('l', 1), ('e', 1)]
+    """ 
+  char_cnt = 0
+  str_dict = {}
+  string = string.replace(' ','')
+  for char in string:
+    char_cnt = string.count(char)
+    str_dict[char] = char_cnt
+  ls = []  
+  for i in (list(string)):
+    if i not in ls:
+      ls.append(i)
+  for i in ls:
+    print ("(%s : %s)" %(i, str_dict[i])),
+  
+
 
 if __name__ == '__main__':
 	"""main function to get user input and call frequency_of_characters function.
     """
-	string=input("enter a string..:")
+	string = input("enter a string..:")
 	if string.isspace():
 		print 'enter a valid string'
 	else:
-		print frequency_of_characters(string)
+		frequency_of_characters(string)
